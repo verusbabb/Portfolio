f8a79cd5a6973f7297d14186ae892ce5ebaab8bb
Steve Babb <steve.babb@outlook.com>
2021-03-31 11:57:19 -0500

    update resume html/script

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-//hide pageview resume to start
-// $('#resume').hide();
-//reveal pageview resume when requested via button; hide 'My Resume' button and replace with 'Download' button.
+
 $('#myResume').click(function (event) {
     event.preventDefault();
         window.open('Docs/Babb_Steve_2020_Resume.pdf', '_blank'); 
     })
-//should be open pdf of resume in alternate window; no working.
-// $('#downloadBtns').on('click', 'downloadResume', function(event) {
-//     event.preventDefault();
-//     window.open('Docs/Babb_Steve_2020_Resume.pdf', '_blank'); //Why is this not functional?
-// })
+
